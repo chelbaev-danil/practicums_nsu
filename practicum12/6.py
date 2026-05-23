b2e1c9a24153a9d2e06d5216d19bccc37403b4eb
chelbaev-danil <91129382+chelbaev-danil@users.noreply.github.com>
2026-05-23 11:08:08 +0700

    Update 6.py

--- practicum12/6.py
+++ practicum12/6.py
@@ -1,7 +1,13 @@
 def degree5(n):
+    '''
+    Рекурсивную функция для нахождения остатка от деления
+    натурального числа a на натуральное число b
+    '''
     if n == 1:
         return 0
     if n < 1 or n % 5 != 0:
         return -1
+        
     res = degree5(n // 5)
+    
     return res + 1 if res != -1 else -1
