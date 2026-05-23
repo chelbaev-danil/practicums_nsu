9d3e6d0cc37334f87d1ca2373bd301c03f680a17
chelbaev-danil <91129382+chelbaev-danil@users.noreply.github.com>
2026-05-23 11:01:17 +0700

    Update 12.py

--- practicum12/12.py
+++ practicum12/12.py
@@ -1,6 +1,8 @@
 def search(a, x):
+    ''' рекурсивную функцию search(a,x), определяющую, имеется ли среди целочисленных значений списка a, число x '''
     if not a:
         return 0
     if a[0] == x:
         return 1
+        
     return search(a[1:], x)
