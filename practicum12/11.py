c3fafc96383b48d1de4bbb5b4f75a51f01262b5c
chelbaev-danil <91129382+chelbaev-danil@users.noreply.github.com>
2026-05-23 10:59:05 +0700

    Add docstring to ind_maxlist function
    Add a docstring for the ind_maxlist function.

--- practicum12/11.py
+++ practicum12/11.py
@@ -1,5 +1,7 @@
 def ind_maxlist(a):
+    '''для вычисления индекса максимального элемента списка целочисленных элементов'''
     if len(a) == 1:
         return 0
+        
     idx = ind_maxlist(a[1:]) + 1
     return 0 if a[0] >= a[idx] else idx
