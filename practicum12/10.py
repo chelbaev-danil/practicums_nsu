184d463035742e79f858891bff56e1f5bfd97217
chelbaev-danil <91129382+chelbaev-danil@users.noreply.github.com>
2026-05-23 10:56:37 +0700

    Update 10.py

--- practicum12/10.py
+++ practicum12/10.py
@@ -1,5 +1,9 @@
 def maxlist(a):
+    ''' рекурсивная функция для вычисления количества цифр натурального числа '''
+    
     if len(a) == 1:
         return a[0]
+        
     m = maxlist(a[1:])
+
     return a[0] if a[0] > m else m
