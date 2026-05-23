18fd83c46261ef584195b10d3642a7d27b4ac2c9
chelbaev-danil <91129382+chelbaev-danil@users.noreply.github.com>
2026-05-23 11:04:29 +0700

    Enhance sum_progress function with docstring
    Added a docstring to the sum_progress function and adjusted the spacing.

--- practicum12/4.py
+++ practicum12/4.py
@@ -1,5 +1,8 @@
 def sum_progress(a1, r, n):
+    '''Рекурсивная функцию для нахождения суммы n членов прогрессии'''
     if n == 1:
         return a1
+        
     curr_n = a1 + (n - 1) * r
+    
     return curr_n + sum_progress(a1, r, n - 1)
