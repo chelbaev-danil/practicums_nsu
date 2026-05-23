b244ff2edc26eed418cc9df590ec801feae539b0
chelbaev-danil <91129382+chelbaev-danil@users.noreply.github.com>
2026-05-23 11:18:15 +0700

    Update 17.py

--- practicum12/17.py
+++ practicum12/17.py
@@ -1,7 +1,10 @@
 def function1(x):
+    '''функция, определяющую, является ли заданное натуральное число x простым'''
     if x < 2: return 0
+        
     def check(d):
         if d * d > x: return 1  # Делителей не нашли
         if x % d == 0: return 0 # Нашли делитель
         return check(d + 1)
+        
     return check(2)
